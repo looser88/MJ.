9ffac246b5832cedbaf40afb6da8f0df43b39d81
looser88 <98893135+looser88@users.noreply.github.com>
2023-03-18 17:34:22 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -128,7 +128,7 @@ async def gen_link_batch(bot, message):
     post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️Generated for filestore.")
     os.remove(f"batchmode_{message.from_user.id}.json")
     file_id, ref = unpack_new_file_id(post.document.file_id)
-    await sts.edit(url:=await get_shortlink(f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}"))
+        await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=BATCH-{file_id}")
            
 
 
